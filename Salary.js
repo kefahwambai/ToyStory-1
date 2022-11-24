6e605c7293283d5509e5d59d11f31b97c9d31c1b
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-25 01:41:04 +0300

    update

--- Salary.js
+++ Salary.js
@@ -1,13 +1,1 @@
-let Salary = {
-    basicSalary: 65000,
-    payAsYouEarn: 11559.35,
-    personalRelief: 2400,
-    NHIF: 1300,
-    NSSF: 400, 
-    benefits: {
-        entertainmentAllowance: 12000,
-        houseAllowance: 25000,
-        fuelAllowance: 10000,
-    }        
-};
 
